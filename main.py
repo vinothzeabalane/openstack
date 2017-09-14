c644782c8e3948a041a39f07e695e33e388bb608
omprakash <dummy.king2015@gmail.com>
2017-09-14 23:20:06 +0530

    [FEA]: Initial commit for Openstack

---
+++ main.py
@@ -0,0 +1,89 @@
+import os  
+import configparser
+from configobj import ConfigObj
+from argparse import ArgumentParser
+
+import replace
+
+class OpenStack:
+    
+    def __init__(self,comp):
+        
+        self.comp = comp
+        self.config1 = configparser.RawConfigParser()
+        self.config1.optionxform=str
+        self.config2 = configparser.RawConfigParser()
+        self.config2.optionxform=str
+        
+        self.env_file = configparser.RawConfigParser()
+        self.env_file.optionxform=str
+        
+        self.config_file = configparser.RawConfigParser()
+        self.config_file.optionxform=str
+        
+    
+    def exec_config_file(self,org_file,replace_file):
+
+        try:
+            config = ConfigObj(org_file)
+            
+            self.config1.read(replace_file)
+            self.config2.read(org_file)
+            
+            for i in self.config1.sections():
+                if i in self.config2.sections():
+                    for k in self.config1.items(i):
+                        if k[0] in str(self.config2.items(i)):
+                            config[i][k[0]] = k[1]
+                            config.write()
+                        else:
+                            config[i][k[0]] = k[1]
+                            config.write()
+                else:
+                    for j in self.config1.items(i):
+                        config[i] = {}
+                        config[i][j[0]] = j[1]
+                        config.write()
+                
+        except Exception as e:
+            print(e)
+            
+            
+    
+    def read_config_file(self):
+        
+        try:
+            self.env_file.read('%s/%s/env.ini' %(os.getcwd(),self.comp))
+            self.config_file.read('%s/%s/config_system.ini' %(os.getcwd(),self.comp))
+            
+            for i in self.config_file.sections():
+                print('Started Installing "%s" section' %i)
+                
+                for k in self.config_file.items(i):
+                    if k[1].startswith("/"):
+                        
+                        self.exec_config_file(k[1], '%s/%s/%s' %(os.getcwd(),self.comp,k[0]))
+                        
+                    elif k[1] == 'admin' or k[1] == 'demo':
+                        print("Setup Client Environment for '%s' user" %k[1])
+                        for m in self.env_file.items(k[1]):
+                            os.environ[m[0]]= m[1]
+                    else:
+                        print("Executing the cmd: %s" %k[1])
+                        os.system(k[1].replace("'''",""))
+            
+        except Exception as e:
+            print(e)
+            
+            
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-c", "--component", type=str,help="component to install")
+    parser.add_argument("-e", "--env", type=str,help="environment")
+    args = parser.parse_args()
+    ins = OpenStack(args.component)
+    rep = replace.ReplaceScript(args.component,args.env)
+    rep.execute_replace('uncomment')
+#     ins.read_config_file()
+#     rep.execute_replace()
+    
---
+++ test.py
@@ -0,0 +1,90 @@
+import os  
+import configparser
+from configobj import ConfigObj
+import json
+
+class ReplaceScript:
+    
+    
+    def __init__(self,cmp,env):
+
+        try:
+            with open("user_input.json") as f:
+                data = json.load(f)
+            
+            self.setup = env
+            self.comp = cmp
+            self.data= data[env]['replace']
+        except Exception as e:
+            print(e)
+        
+        
+    def execute_replace(self,condition=None):
+        
+        try:
+            res =  os.listdir(os.getcwd()+'/%s' %self.comp)
+            for file in res:
+                
+                print("Executing the file for %s" %file)
+                self.config = configparser.RawConfigParser()
+                self.config.optionxform=str
+                self.config.read(os.getcwd()+'/%s/%s' %(self.comp,file))
+                
+                self.config1 = ConfigObj(os.getcwd()+'/%s/%s' %(self.comp,file))
+                
+                
+                if condition == 'uncomment':
+                    self.uncomment()
+                else:
+                    self.comment()
+           
+        except Exception as e:
+            print(e)
+                
+        
+        
+    def uncomment(self,file="placement.ini"):
+        try:
+            print("Executing the file for %s" %file)
+            self.config = configparser.RawConfigParser()
+            self.config.optionxform=str
+            self.config.read(os.getcwd()+'/%s/%s' %(self.comp,file))
+            
+            self.config1 = ConfigObj(os.getcwd()+'/%s/%s' %(self.comp,file))
+            
+            for l in self.data.keys():
+                for i in self.config.sections():
+                    for k in self.config.items(i):
+                        if l in str(k[1]):
+                            val= self.data[l]
+                            print("Expression changed as '%s'" % val)
+                            self.config1[i][k[0]] = self.config1[i][k[0]].replace(l,val)
+                            self.config1.write()
+        except Exception as e:
+            if 'File contains no section headers.' in str(e):
+                pass
+            else:
+                print(e)        
+        
+    
+    def comment(self):
+        try:
+            for l in self.data.values():
+                for i in self.config.sections():
+                    for k in self.config.items(i):
+                        if l in str(k[1]):
+                            val = (list(self.data.keys())[list(self.data.values()).index(l)])
+                            print("Expression changed as '%s' " % val )
+                            self.config1[i][k[0]] = self.config1[i][k[0]].replace(l,val)
+                            self.config1.write()
+        except Exception as e:
+            print(e)
+
+            
+if __name__ == "__main__":
+    ins = ReplaceScript(cmp='controller',env='setup1')
+#     ins.comment("config_system.ini")
+    ins.uncomment("placement.ini")
+#     ins.execute_replace('uncomment')
+#     ins.execute_replace()
+    
